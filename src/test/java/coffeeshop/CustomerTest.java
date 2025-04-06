2e134cd64b29799abaf556af78976330110485a8
Benjos <benjamin@bourge.be>
2025-04-06 12:14:50 +0100

    Unit Test for Order in Customer

--- src/test/java/coffeeshop/CustomerTest.java
+++ src/test/java/coffeeshop/CustomerTest.java
@@ -1,6 +1,7 @@
 package coffeeshop;
 
 import coffeeshop.model.Customer;
+import coffeeshop.model.Order;
 import org.junit.jupiter.api.Test;
 
 import java.util.Objects;
@@ -14,6 +15,19 @@ class CustomerTest {
         Customer customer = new Customer("Frodo");
 
         if (!Objects.equals(customer.getId(), "Frodo"))
+            assertTrue(false);
+        assertTrue(true);
+    }
+
+    @Test
+    void setOrderToCustomer()
+    {
+        Customer customer = new Customer("Frodo");
+
+        customer.createOrder();
+        if (customer.getCurrentOrder() == null)
+            assertTrue(false);
+        if (!Objects.equals(customer.getCurrentOrder().getCustomerId(), customer.getId()))
             assertTrue(false);
         assertTrue(true);
     }
